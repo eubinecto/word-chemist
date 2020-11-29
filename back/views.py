6930807e99482e1439cd9804aee371d7cf354f47
Eu-Bin KIM <tlrndk123@gmail.com>
2020-11-29 15:57:11 +0900

    [#1] new api:  similar_by_word

--- back/views.py
+++ back/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from back.errors import InvalidRequestError
-from back.utils import load_fasttext_model, all_options, add, sub, similar_by_vec
+from back.utils import load_fasttext_model, all_options, add, sub, similar_by_vec, similar_by_word
 
 app = Flask(__name__)
 # this should be setup
@@ -29,11 +29,11 @@ def api_all_options():
 
 @app.route("/word_chemist/add_or_sub")
 def api_add_or_sub():
+    global FASTTEXT_MODEL
     op = request.args.get("op")
     first = request.args.get("first")
     second = request.args.get("second")
     top_n = request.args.get("top_n", type=int)
-    global FASTTEXT_MODEL
     if op not in VALID_OPS:
         raise InvalidRequestError
     elif op == "add":
@@ -42,3 +42,11 @@ def api_add_or_sub():
         res = sub(FASTTEXT_MODEL, first, second)
     sims = similar_by_vec(FASTTEXT_MODEL, res, top_n)
     return jsonify(sims)
+
+
+@app.route("/word_chemist/similar_by_word")
+def api_similar_by_word():
+    global FASTTEXT_MODEL
+    word = request.args.get("word")
+    top_n = request.args.get("top_n", type=int)
+    return similar_by_word(FASTTEXT_MODEL, word, top_n)
