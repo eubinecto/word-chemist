3e28d9eb2bbd2547d54666945300f3cf98ec6b47
Eu-Bin KIM <tlrndk123@gmail.com>
2020-11-29 19:19:57 +0900

    CORS access fixed

--- back/views.py
+++ back/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
 from back.errors import InvalidRequestError
 from back.utils import load_fasttext_model, load_choices, add, sub, similar_by_vec, similar_by_word, cos_dist
 import random
@@ -26,6 +28,7 @@ def handle_invalid_request(error):
 
 
 @app.route('/word_chemist/all_choices')
+@cross_origin()
 def api_all_choices():
     global CHOICES
     """
@@ -35,6 +38,7 @@ def api_all_choices():
 
 
 @app.route("/word_chemist/add_or_sub")
+@cross_origin()
 def api_add_or_sub():
     global FASTTEXT_MODEL
     op = request.args.get("op")
@@ -52,6 +56,7 @@ def api_add_or_sub():
 
 
 @app.route("/word_chemist/similar_by_word")
+@cross_origin()
 def api_similar_by_word():
     global FASTTEXT_MODEL
     word = request.args.get("word")
@@ -61,6 +66,7 @@ def api_similar_by_word():
 
 
 @app.route("/word_chemist/src_dest")
+@cross_origin()
 def api_src_dest():
     global SRC_DEST
     pair = random.choice(SRC_DEST)
@@ -68,6 +74,7 @@ def api_src_dest():
 
 
 @app.route("/word_chemist/cos_dist")
+@cross_origin()
 def api_cos_dist():
     global FASTTEXT_MODEL
     first = request.args.get("first")
